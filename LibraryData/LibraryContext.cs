c56d1e4bf0f2d50e9509fedfa6065feab78fe1c1
Ernesto Murillo <emurillo@email.neit.edu>
2018-01-14 19:56:47 -0500

    initial commit

---
+++ LibraryData/LibraryContext.cs
@@ -0,0 +1,12 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryData
+{
+    public class LibraryContext: DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
---
+++ LibraryData/Migrations/20180114210435_Initial migration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryData.Migrations
+{
+    public partial class Initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patrons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    TelephoneNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patrons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patrons");
+        }
+    }
+}
